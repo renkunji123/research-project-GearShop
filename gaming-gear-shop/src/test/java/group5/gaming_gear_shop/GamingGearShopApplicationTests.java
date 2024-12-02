6dcff83fea421240fd6007e48f65532380fdc305
Hong Son <hongsonn539@gmail.com>
2024-12-02 14:00:11 +0700

    init: initial commit with project structure

---
+++ gaming-gear-shop/src/test/java/group5/gaming_gear_shop/GamingGearShopApplicationTests.java
@@ -0,0 +1,13 @@
+package group5.gaming_gear_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingGearShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
