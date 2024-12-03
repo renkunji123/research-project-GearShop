850963f5d2dbb1f975844e9987c0fe5e4216974a
Hong Son <hongsonn539@gmail.com>
2024-12-04 02:00:00 +0700

    chore: update exception classes

--- gaming-gear-shop/src/main/java/group5/gaming_gear_shop/exception/handler/ErrorCode.java
+++ gaming-gear-shop/src/main/java/group5/gaming_gear_shop/exception/handler/ErrorCode.java
@@ -6,7 +6,34 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    PRODUCT_NOT_FOUND(400, "Product Not Found!");
+    //Category
+    CATEGORY_NOT_FOUND(400, "Category not found."),
+    EXISTED_CATEGORY(400, "Category already exists."),
+    //Brand
+    BRAND_NOT_FOUND(400, "Brand not found."),
+    EXISTED_BRAND(400, "Brand already exists."),
+    //Product
+    PRODUCT_NOT_FOUND(400, "Product not found."),
+    EXISTED_PRODUCT(400, "Product already exists."),
+    OUT_OF_STOCK(400, "Product is out of stock"),
+    //Cart
+    PRODUCT_EXISTED_IN_CART(400, "Product is already in the cart"),
+    INVALID_QUANTITY(400, "Invalid quantity."),
+    CART_NOT_FOUND(400, "Cart not found."),
+    CART_ITEM_NOT_FOUND(400, "Cart item not found"),
+    //Payment
+    PAYMENT_METHOD_NOT_FOUND(400, "Payment method not found."),
+    //Order
+    ORDER_NOT_FOUND(400, "Order not found."),
+    ORDER_CAN_NOT_BE_CANCELLED(400, "Order cannot be canceled in current status"),
+    NO_CART_ITEM_SELECTED(400, "No cart item selected."),
+    //User
+    USER_NOT_FOUND(400, "User not found."),
+    EXISTED_EMAIL(400, "Email is already in use."),
+    INCORRECT_PASSWORD(400, "Password is incorrect."),
+    INVALID_EMAIL_OR_PASSWORD(400, "Invalid email or password."),
+    UNAUTHORIZED_ORDER_ACCESS(400, "You do not have permission to perform this function."),
+    ;
 
     private final int code;
     private final String message;
--- gaming-gear-shop/src/main/java/group5/gaming_gear_shop/exception/handler/GlobalExceptionHandler.java
+++ gaming-gear-shop/src/main/java/group5/gaming_gear_shop/exception/handler/GlobalExceptionHandler.java
@@ -2,6 +2,25 @@ package group5.gaming_gear_shop.exception.handler;
 
 
 import group5.gaming_gear_shop.dto.ApiResponse;
+import group5.gaming_gear_shop.exception.brand.BrandNotFoundException;
+import group5.gaming_gear_shop.exception.brand.ExistedBrandException;
+import group5.gaming_gear_shop.exception.cart.CartItemNotFoundException;
+import group5.gaming_gear_shop.exception.cart.CartNotFoundException;
+import group5.gaming_gear_shop.exception.cart.InvalidQuantityException;
+import group5.gaming_gear_shop.exception.cart.ProductExistedInCartException;
+import group5.gaming_gear_shop.exception.category.CategoryNotFoundException;
+import group5.gaming_gear_shop.exception.category.ExistedCategoryException;
+import group5.gaming_gear_shop.exception.order.NoCartItemSelectedException;
+import group5.gaming_gear_shop.exception.order.OrderCannotBeCancelledException;
+import group5.gaming_gear_shop.exception.order.OrderNotFoundException;
+import group5.gaming_gear_shop.exception.payment.PaymentMethodNotFoundException;
+import group5.gaming_gear_shop.exception.product.ExistedProductException;
+import group5.gaming_gear_shop.exception.product.OutOfStockException;
+import group5.gaming_gear_shop.exception.product.ProductNotFoundException;
+import group5.gaming_gear_shop.exception.user.IncorrectPasswordException;
+import group5.gaming_gear_shop.exception.user.InvalidEmailOrPasswordException;
+import group5.gaming_gear_shop.exception.user.UnauthorizedOrderAccessException;
+import group5.gaming_gear_shop.exception.user.UserNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,7 +28,36 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler({
+        //Common exceptions
         RuntimeException.class,
+        IllegalStateException.class,
+        //Category
+        CategoryNotFoundException.class,
+        ExistedCategoryException.class,
+        //Brand
+        BrandNotFoundException.class,
+        ExistedBrandException.class,
+        //Product
+        ProductNotFoundException.class,
+        ExistedProductException.class,
+        OutOfStockException.class,
+        //Cart
+        CartNotFoundException.class,
+        CartItemNotFoundException.class,
+        InvalidQuantityException.class,
+        ProductExistedInCartException.class,
+        //Payment
+        PaymentMethodNotFoundException.class,
+        //Order
+        OrderNotFoundException.class,
+        OrderCannotBeCancelledException.class,
+        NoCartItemSelectedException.class,
+        //User
+        UserNotFoundException.class,
+        InvalidEmailOrPasswordException.class,
+        ExistedBrandException.class,
+        IncorrectPasswordException.class,
+        UnauthorizedOrderAccessException.class,
     })
     ResponseEntity<ApiResponse<?>> handleCustomExceptions(RuntimeException ex) {
         if (ex instanceof BaseCustomException customEx) {
